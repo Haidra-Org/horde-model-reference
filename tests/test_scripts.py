755d556c3b762676d9c2ae13d08cb7da99902084
tazlin <tazlin.on.github@gmail.com>
2023-06-05 17:50:49 -0400

    feat: add `ReferenceDownloadManager` class

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -1,11 +1,12 @@
-from horde_model_reference.legacy.download_live_legacy_dbs import download_all_models
+from horde_model_reference.legacy.download_live_legacy_dbs import ReferenceDownloadManager
 from horde_model_reference.legacy.validate_sd import validate_legacy_stable_diffusion_db
 from horde_model_reference.meta_consts import MODEL_REFERENCE_CATEGORIES
 from horde_model_reference.path_consts import LEGACY_REFERENCE_FOLDER, get_model_reference_file_path
 
 
 def test_download_all_models():
-    download_models = download_all_models(override_existing=True)
+    reference_download_manager = ReferenceDownloadManager()
+    download_models = reference_download_manager.download_all_models(override_existing=True)
     assert len(download_models) == 8
 
 
