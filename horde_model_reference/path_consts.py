4d1214f74c46380bec59358e6b4a162797886653
tazlin <tazlin.on.github@gmail.com>
2023-06-05 11:19:40 -0400

    refactor: improve structure, use fewer literals, support AIWORKER_CACHE_HOME

--- horde_model_reference/path_consts.py
+++ horde_model_reference/path_consts.py
@@ -1,11 +1,37 @@
 """Constants, especially those to do with paths or network locations, for the horde_model_reference package."""
 
+import os
 from pathlib import Path
 from urllib.parse import urlparse
 
 from horde_model_reference import MODEL_REFERENCE_CATEGORIES
 
 PACKAGE_NAME = "horde_model_reference"
+
+BASE_PATH: Path = Path(__file__).parent
+"""The base path for all model reference files. Will be based in AIWORKER_CACHE_HOME if set, otherwise will be based in
+ this package's install location (IE, in site-packages.)"""
+
+AIWORKER_CACHE_HOME = os.getenv("AIWORKER_CACHE_HOME")
+"""The default location for all AI-Horde-Worker cache (model) files."""
+
+if AIWORKER_CACHE_HOME:
+    BASE_PATH = Path(AIWORKER_CACHE_HOME).joinpath(PACKAGE_NAME)
+
+LOG_FOLDER: Path = BASE_PATH.joinpath("logs")
+LOG_FOLDER.mkdir(parents=True, exist_ok=True)
+
+LEGACY_REFERENCE_FOLDER: Path = BASE_PATH.joinpath("legacy")
+"""The path to the legacy model reference folder."""
+LEGACY_REFERENCE_FOLDER.mkdir(parents=True, exist_ok=True)
+
+DEFAULT_SHOWCASE_FOLDER_NAME: str = "showcase"
+"""The default name of the stable diffusion showcase folder. If you need the path, use `SHOWCASE_FOLDER_PATH`."""
+
+SHOWCASE_FOLDER_PATH: Path = BASE_PATH.joinpath(DEFAULT_SHOWCASE_FOLDER_NAME)
+"""The path to the stable diffusion showcase folder."""
+SHOWCASE_FOLDER_PATH.mkdir(parents=True, exist_ok=True)
+
 
 GITHUB_REPO_OWNER = "Haidra-Org"
 GITHUB_REPO_NAME = "AI-Horde-image-model-reference"
@@ -16,38 +42,15 @@ GITHUB_REPO_URL: str = (
 )
 """The base URL to the live GitHub repo used to power the horde."""
 
-LEGACY_MODEL_GITHUB_URLS = {
-    MODEL_REFERENCE_CATEGORIES.BLIP: urlparse(GITHUB_REPO_URL + "blip.json").geturl(),
-    MODEL_REFERENCE_CATEGORIES.CLIP: urlparse(GITHUB_REPO_URL + "clip.json").geturl(),
-    MODEL_REFERENCE_CATEGORIES.CODEFORMER: urlparse(GITHUB_REPO_URL + "codeformer.json").geturl(),
-    MODEL_REFERENCE_CATEGORIES.CONTROLNET: urlparse(GITHUB_REPO_URL + "controlnet.json").geturl(),
-    MODEL_REFERENCE_CATEGORIES.ESRGAN: urlparse(GITHUB_REPO_URL + "esrgan.json").geturl(),
-    MODEL_REFERENCE_CATEGORIES.GFPGAN: urlparse(GITHUB_REPO_URL + "gfpgan.json").geturl(),
-    MODEL_REFERENCE_CATEGORIES.SAFETY_CHECKER: urlparse(GITHUB_REPO_URL + "safety_checker.json").geturl(),
-    MODEL_REFERENCE_CATEGORIES.STABLE_DIFFUSION: urlparse(GITHUB_REPO_URL + "stable_diffusion.json").geturl(),
-}
-"""A lookup of all the fully qualified URLs to the live model reference files."""
-
-
-BASE_PATH: Path = Path(__file__).parent
-"""The base path to the image database folder. Should end in `horde_model_reference/`."""
-
-LEGACY_REFERENCE_FOLDER: Path = BASE_PATH.joinpath("legacy")
-"""The path to the legacy model reference folder."""
-
-
-DEFAULT_SHOWCASE_FOLDER_NAME: str = "showcase"
-"""The default name of the stable diffusion showcase folder. If you need the path, use `SHOWCASE_FOLDER_PATH`."""
-
-
-SHOWCASE_FOLDER_PATH: Path = BASE_PATH.joinpath(DEFAULT_SHOWCASE_FOLDER_NAME)
-"""The path to the stable diffusion showcase folder."""
-
+LEGACY_MODEL_GITHUB_URLS = {}
+"""A lookup of all the fully qualified file URLs to the given model reference."""
 
 _MODEL_REFERENCE_FILENAMES: dict[MODEL_REFERENCE_CATEGORIES, str] = {}
 
 for category in MODEL_REFERENCE_CATEGORIES:
-    _MODEL_REFERENCE_FILENAMES[category] = f"{category}.json"
+    filename = f"{category}.json"
+    _MODEL_REFERENCE_FILENAMES[category] = filename
+    LEGACY_MODEL_GITHUB_URLS[category] = urlparse(GITHUB_REPO_URL + filename).geturl()
 
 
 def get_model_reference_filename(
