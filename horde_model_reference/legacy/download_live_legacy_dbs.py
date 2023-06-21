d0439689839f0c7194b7a650f10e249444b4931f
tazlin <tazlin.on.github@gmail.com>
2023-06-21 16:34:21 -0400

    refactor: name download funcs to reflect legacy nature

--- horde_model_reference/legacy/download_live_legacy_dbs.py
+++ horde_model_reference/legacy/download_live_legacy_dbs.py
@@ -1,4 +1,5 @@
 import pathlib
+from pathlib import Path
 
 import requests
 from loguru import logger
@@ -7,6 +8,7 @@ from horde_model_reference.meta_consts import MODEL_REFERENCE_CATEGORIES
 from horde_model_reference.path_consts import (
     LEGACY_MODEL_GITHUB_URLS,
     LEGACY_REFERENCE_FOLDER,
+    LEGACY_REFERENCE_FOLDER_NAME,
     get_model_reference_file_path,
 )
 
@@ -15,17 +17,26 @@ class ReferenceDownloadManager:
     proxy_url: str = ""
     """The URL to use as a proxy for downloading files. If empty, no proxy will be used."""
 
-    def __init__(self, proxy_url: str = "") -> None:
+    base_path: str | Path = LEGACY_REFERENCE_FOLDER
+
+    def __init__(
+        self,
+        *,
+        base_path: str | Path = LEGACY_REFERENCE_FOLDER,
+        proxy_url: str = "",
+    ) -> None:
+        self.base_path = base_path
+        self.legacy_path = Path(self.base_path).joinpath(LEGACY_REFERENCE_FOLDER_NAME)
         self.proxy_url = proxy_url
 
-    def download_model_reference(
+    def download_legacy_model_reference(
         self,
         *,
         model_category_name: MODEL_REFERENCE_CATEGORIES,
         override_existing: bool = False,
     ) -> pathlib.Path | None:
         response = requests.get(self.proxy_url + LEGACY_MODEL_GITHUB_URLS[model_category_name])
-        target_file_path = get_model_reference_file_path(model_category_name, base_path=LEGACY_REFERENCE_FOLDER)
+        target_file_path = get_model_reference_file_path(model_category_name, base_path=self.legacy_path)
 
         if target_file_path.exists() and not override_existing:
             return None
@@ -35,7 +46,7 @@ class ReferenceDownloadManager:
             f.write(response.content)
         return target_file_path
 
-    def download_all_model_references(
+    def download_all_legacy_model_references(
         self,
         *,
         override_existing: bool = False,
@@ -50,21 +61,20 @@ class ReferenceDownloadManager:
         """
         downloaded_files: dict[MODEL_REFERENCE_CATEGORIES, pathlib.Path | None] = {}
         for model_category_name in MODEL_REFERENCE_CATEGORIES:
-            downloaded_files[model_category_name] = self.download_model_reference(
+            downloaded_files[model_category_name] = self.download_legacy_model_reference(
                 model_category_name=model_category_name,
                 override_existing=override_existing,
             )
 
         return downloaded_files
 
-    def download_all_models(self, override_existing: bool = False, proxy_url: str = ""):
-        logger.error("This method is deprecated. Use `download_all_model_references` instead.")
-        self.proxy_url = proxy_url
-        self.download_all_model_references(override_existing=override_existing)
-
-    def read_all_model_references(self, *, redownload_all: bool = False):
+    def read_all_legacy_model_references(
+        self,
+        *,
+        redownload_all: bool = False,
+    ) -> dict[MODEL_REFERENCE_CATEGORIES, Path | None]:
         """Read all legacy model reference files from disk, optionally redownloading them first."""
-        self.download_all_model_references(override_existing=redownload_all)
+        return self.download_all_legacy_model_references(override_existing=redownload_all)
 
 
 def download_all_models(
@@ -73,9 +83,9 @@ def download_all_models(
 ) -> dict[MODEL_REFERENCE_CATEGORIES, pathlib.Path | None]:
     reference_dm = ReferenceDownloadManager(proxy_url=proxy_url)
     logger.error("This method is deprecated. Use `download_all_model_references` instead.")
-    return reference_dm.download_all_model_references(override_existing=override_existing)
+    return reference_dm.download_all_legacy_model_references(override_existing=override_existing)
 
 
 if __name__ == "__main__":
     reference_download_manager = ReferenceDownloadManager()
-    reference_download_manager.download_all_model_references(override_existing=True)
+    reference_download_manager.download_all_legacy_model_references(override_existing=True)
