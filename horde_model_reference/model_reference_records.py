6f353812bde2999eed3ee3c243642b4d11f7c0d6
tazlin <tazlin.on.github@gmail.com>
2023-08-20 08:26:31 -0400

    refactor: stream line reference records
    In particular, the use of RootModel and computing the properties (styles, tags, etc) should make for a slightly clearer intent on what I was trying to accomplish

--- horde_model_reference/model_reference_records.py
+++ horde_model_reference/model_reference_records.py
@@ -1,16 +1,24 @@
 """The model database pydantic models and associate enums/lookups."""
 from __future__ import annotations
 
+import urllib.parse
 from collections.abc import Mapping
 
-from pydantic import BaseModel, ConfigDict, model_validator
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    PrivateAttr,
+    RootModel,
+    model_validator,
+)
 
 from horde_model_reference import (
     MODEL_PURPOSE,
-    MODEL_REFERENCE_CATEGORIES,
-    MODEL_STYLES,
-    STABLE_DIFFUSION_BASELINE_CATEGORIES,
+    MODEL_REFERENCE_CATEGORY,
+    MODEL_STYLE,
+    STABLE_DIFFUSION_BASELINE_CATEGORY,
 )
+from horde_model_reference.meta_consts import CONTROLNET_STYLE
 
 
 class DownloadRecord(BaseModel):  # TODO Rename? (record to subrecord?)
@@ -30,13 +38,12 @@ class Generic_ModelRecord(BaseModel):
     # TODO forbid extra?
     name: str
     """The name of the model."""
-    description: str | None
+    description: str | None = None
     """A short description of the model."""
-    version: str | None
+    version: str | None = None
     """The version of the  model (not the version of SD it is based on, see `baseline` for that info)."""
-    style: MODEL_STYLES | str | None  # TODO remove str
-    """The style of the model."""
-    config: Mapping[str, list[DownloadRecord]]
+
+    config: dict[str, list[DownloadRecord]]
     """A dictionary of any configuration files and information on where to download the model file(s)."""
 
     purpose: MODEL_PURPOSE
@@ -50,7 +57,7 @@ class StableDiffusion_ModelRecord(Generic_ModelRecord):
 
     inpainting: bool | None = False
     """If this is an inpainting model or not."""
-    baseline: STABLE_DIFFUSION_BASELINE_CATEGORIES
+    baseline: STABLE_DIFFUSION_BASELINE_CATEGORY
     """The model on which this model is based."""
     tags: list[str] | None = []
     """Any tags associated with the model which may be useful for searching."""
@@ -65,6 +72,9 @@ class StableDiffusion_ModelRecord(Generic_ModelRecord):
     nsfw: bool
     """Whether the model is NSFW or not."""
 
+    style: MODEL_STYLE | None = None
+    """The style of the model."""
+
     @model_validator(mode="after")  # type: ignore # FIXME
     def validator_set_arrays_to_empty_if_none(self) -> StableDiffusion_ModelRecord:
         """Set any `None` values to empty lists."""
@@ -78,12 +88,16 @@ class StableDiffusion_ModelRecord(Generic_ModelRecord):
 
 
 class CLIP_ModelRecord(Generic_ModelRecord):
-    pretrained_name: str | None
+    pretrained_name: str | None = None
     # TODO docstring
 
 
-class Generic_ModelReference(BaseModel):
-    models: Mapping[str, Generic_ModelRecord]
+class ControlNet_ModelRecord(Generic_ModelRecord):
+    style: CONTROLNET_STYLE | None = None
+
+
+class Generic_ModelReference(RootModel[Mapping[str, Generic_ModelRecord]]):
+    root: Mapping[str, Generic_ModelRecord]
     """A dictionary of all the models."""
 
 
@@ -92,69 +106,126 @@ class StableDiffusion_ModelReference(Generic_ModelReference):
 
     model_config = ConfigDict(extra="forbid")
 
-    baseline: Mapping[STABLE_DIFFUSION_BASELINE_CATEGORIES, int]
+    _baseline: dict[STABLE_DIFFUSION_BASELINE_CATEGORY, int] = PrivateAttr(default_factory=dict)
     """A dictionary of all the baseline types and how many models use them."""
-    styles: Mapping[MODEL_STYLES, int]
+    _styles: dict[MODEL_STYLE, int] = PrivateAttr(default_factory=dict)
     """A dictionary of all the styles and how many models use them."""
-    tags: Mapping[str, int]
+    _tags: dict[str, int] = PrivateAttr(default_factory=dict)
     """A dictionary of all the tags and how many models use them."""
-    models_hosts: Mapping[str, int]
+    _models_hosts: dict[str, int] = PrivateAttr(default_factory=dict)
     """A dictionary of all the model hosts and how many models use them."""
-    models: Mapping[str, StableDiffusion_ModelRecord]
+    root: dict[str, StableDiffusion_ModelRecord]
     """A dictionary of all the models."""
 
+    _models_dict_hash: int | None = None
 
-def create_stablediffusion_modelreference(
-    models: Mapping[str, StableDiffusion_ModelRecord],
-) -> StableDiffusion_ModelReference:
-    """Create a StableDiffusion_ModelReference from a mapping of {str: StableDiffusion_ModelRecords}."""
-    baseline_categories: dict[STABLE_DIFFUSION_BASELINE_CATEGORIES, int] = {}
-    styles: dict[MODEL_STYLES, int] = {}
-    tags: dict[str, int] = {}
-    download_hosts: dict[str, int] = {}
-    for model in models.values():
-        baseline_categories[model.baseline] = baseline_categories.get(model.baseline, 0) + 1
-        if model.style is not None and model.style in MODEL_STYLES.__members__:
-            styles[MODEL_STYLES(model.style)] = styles.get(MODEL_STYLES(model.style), 0) + 1
-        if model.tags is not None:
-            for tag in model.tags:
-                tags[tag] = tags.get(tag, 0) + 1
-        for host in model.config:
-            download_hosts[host] = download_hosts.get(host, 0) + 1
-    return StableDiffusion_ModelReference(
-        baseline=baseline_categories,
-        styles=styles,
-        tags=tags,
-        models_hosts=download_hosts,
-        models=models,
-    )
+    def check_was_models_modified(self) -> bool:
+        """Check if the models dictionary has been modified since the last time it was hashed.
+
+        Note: this only checks if the keys differ, and does not check into the value objects.
+        You should call `rebuild_metadata` if you want to be sure the metadata is up to date.
+        """
+
+        if not self._models_dict_hash or self._models_dict_hash != hash(frozenset(self.root.keys())):
+            self._models_dict_hash = hash(frozenset(self.root.keys()))
+            self.rebuild_metadata()
+            return True
+
+        return False
+
+    @property
+    def baseline(self) -> dict[STABLE_DIFFUSION_BASELINE_CATEGORY, int]:
+        """Return a dictionary of all the baseline types and how many models use them."""
+        self.check_was_models_modified()
+        return self._baseline
+
+    @property
+    def styles(self) -> dict[MODEL_STYLE, int]:
+        """Return a dictionary of all the styles and how many models use them."""
+        self.check_was_models_modified()
+        return self._styles
+
+    @property
+    def tags(self) -> dict[str, int]:
+        """Return a dictionary of all the tags and how many models use them."""
+        self.check_was_models_modified()
+        return self._tags
+
+    @property
+    def models_hosts(self) -> dict[str, int]:
+        """Return a dictionary of all the model hosts and how many models use them."""
+        self.check_was_models_modified()
+        return self._models_hosts
+
+    @property
+    def models_names(self) -> set[str]:
+        """Return a list of all the model names."""
+        return set(self.root.keys())
+
+    def rebuild_metadata(self) -> None:
+        # Initialize empty dictionaries to store metadata
+        self._baseline = {}  # Dictionary to count models by baseline
+        self._styles = {}  # Dictionary to count models by style
+        self._tags = {}  # Dictionary to count models by tag
+        self._models_hosts = {}  # Dictionary to count models by host
+
+        # Iterate over all models in the dictionary
+        for model in self.root.values():
+            # Count models by baseline
+            self._baseline[model.baseline] = self._baseline.get(model.baseline, 0) + 1
+
+            # Count models by style, if the style is valid
+            if model.style is not None and model.style in MODEL_STYLE.__members__:
+                self._styles[MODEL_STYLE(model.style)] = self._styles.get(MODEL_STYLE(model.style), 0) + 1
+
+            # Count models by tag
+            if model.tags is not None:
+                for tag in model.tags:
+                    self._tags[tag] = self._tags.get(tag, 0) + 1
+
+            # Count models by host, based on the download URLs in the config
+            for entries in model.config.items():
+                entry_name, data = entries
+
+                # Look for the "download" entry in the config
+                if entry_name == "download":
+                    for download_entry in data:
+                        # Extract the host from the download URL
+                        host = urllib.parse.urlparse(download_entry.file_url).netloc
+                        # Count models by host
+                        self._models_hosts[host] = self._models_hosts.get(host, 0) + 1
 
 
 class CLIP_ModelReference(Generic_ModelReference):
-    models: Mapping[str, CLIP_ModelRecord]
+    root: Mapping[str, CLIP_ModelRecord]
     """A dictionary of all the models."""
 
 
-MODEL_REFERENCE_RECORD_TYPE_LOOKUP: dict[MODEL_REFERENCE_CATEGORIES, type[Generic_ModelRecord]] = {
-    MODEL_REFERENCE_CATEGORIES.stable_diffusion: StableDiffusion_ModelRecord,
-    MODEL_REFERENCE_CATEGORIES.controlnet: Generic_ModelRecord,
-    MODEL_REFERENCE_CATEGORIES.clip: CLIP_ModelRecord,
-    MODEL_REFERENCE_CATEGORIES.blip: Generic_ModelRecord,
-    MODEL_REFERENCE_CATEGORIES.esrgan: Generic_ModelRecord,
-    MODEL_REFERENCE_CATEGORIES.gfpgan: Generic_ModelRecord,
-    MODEL_REFERENCE_CATEGORIES.safety_checker: Generic_ModelRecord,
-    MODEL_REFERENCE_CATEGORIES.codeformer: Generic_ModelRecord,
+class ControlNet_ModelReference(Generic_ModelReference):
+    root: Mapping[str, ControlNet_ModelRecord]
+    """A dictionary of all the models."""
+
+
+MODEL_REFERENCE_RECORD_TYPE_LOOKUP: dict[MODEL_REFERENCE_CATEGORY, type[Generic_ModelRecord]] = {
+    MODEL_REFERENCE_CATEGORY.stable_diffusion: StableDiffusion_ModelRecord,
+    MODEL_REFERENCE_CATEGORY.controlnet: ControlNet_ModelRecord,
+    MODEL_REFERENCE_CATEGORY.clip: CLIP_ModelRecord,
+    MODEL_REFERENCE_CATEGORY.blip: Generic_ModelRecord,
+    MODEL_REFERENCE_CATEGORY.esrgan: Generic_ModelRecord,
+    MODEL_REFERENCE_CATEGORY.gfpgan: Generic_ModelRecord,
+    MODEL_REFERENCE_CATEGORY.safety_checker: Generic_ModelRecord,
+    MODEL_REFERENCE_CATEGORY.codeformer: Generic_ModelRecord,
 }
 """A lookup for the model record type based on the model category. See also `MODEL_REFERENCE_TYPE_LOOKUP`."""
 
-MODEL_REFERENCE_TYPE_LOOKUP: dict[MODEL_REFERENCE_CATEGORIES, type[Generic_ModelReference]] = {
-    MODEL_REFERENCE_CATEGORIES.stable_diffusion: StableDiffusion_ModelReference,
-    MODEL_REFERENCE_CATEGORIES.controlnet: Generic_ModelReference,
-    MODEL_REFERENCE_CATEGORIES.clip: CLIP_ModelReference,
-    MODEL_REFERENCE_CATEGORIES.blip: Generic_ModelReference,
-    MODEL_REFERENCE_CATEGORIES.esrgan: Generic_ModelReference,
-    MODEL_REFERENCE_CATEGORIES.gfpgan: Generic_ModelReference,
-    MODEL_REFERENCE_CATEGORIES.safety_checker: Generic_ModelReference,
-    MODEL_REFERENCE_CATEGORIES.codeformer: Generic_ModelReference,
+MODEL_REFERENCE_TYPE_LOOKUP: dict[MODEL_REFERENCE_CATEGORY, type[Generic_ModelReference]] = {
+    MODEL_REFERENCE_CATEGORY.stable_diffusion: StableDiffusion_ModelReference,
+    MODEL_REFERENCE_CATEGORY.controlnet: ControlNet_ModelReference,
+    MODEL_REFERENCE_CATEGORY.clip: CLIP_ModelReference,
+    MODEL_REFERENCE_CATEGORY.blip: Generic_ModelReference,
+    MODEL_REFERENCE_CATEGORY.esrgan: Generic_ModelReference,
+    MODEL_REFERENCE_CATEGORY.gfpgan: Generic_ModelReference,
+    MODEL_REFERENCE_CATEGORY.safety_checker: Generic_ModelReference,
+    MODEL_REFERENCE_CATEGORY.codeformer: Generic_ModelReference,
 }
 """A lookup for the model reference type based on the model category. See also `MODEL_REFERENCE_RECORD_TYPE_LOOKUP`."""
