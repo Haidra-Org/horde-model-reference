e595b90358e9daaaf5b6a330af36cc437c8e6b84
tazlin <tazlin.on.github@gmail.com>
2023-08-20 08:27:53 -0400

    feat: ModelReference and LegacyDownload managers
    These will hopefully serve as more straight forward interfaces to the library.

--- horde_model_reference/legacy/download_live_legacy_dbs.py
+++ horde_model_reference/legacy/download_live_legacy_dbs.py
@@ -1,91 +1,23 @@
 import pathlib
-from pathlib import Path
+import warnings
 
-import requests
-from loguru import logger
-
-from horde_model_reference.meta_consts import MODEL_REFERENCE_CATEGORIES
-from horde_model_reference.path_consts import (
-    BASE_PATH,
-    LEGACY_MODEL_GITHUB_URLS,
-    LEGACY_REFERENCE_FOLDER_NAME,
-    get_model_reference_file_path,
-)
-
-
-class ReferenceDownloadManager:
-    proxy_url: str = ""
-    """The URL to use as a proxy for downloading files. If empty, no proxy will be used."""
-
-    base_path: str | Path = BASE_PATH
-
-    def __init__(
-        self,
-        *,
-        base_path: str | Path = BASE_PATH,
-        proxy_url: str = "",
-    ) -> None:
-        self.base_path = base_path
-        self.legacy_path = Path(self.base_path).joinpath(LEGACY_REFERENCE_FOLDER_NAME)
-        self.proxy_url = proxy_url
-
-    def download_legacy_model_reference(
-        self,
-        *,
-        model_category_name: MODEL_REFERENCE_CATEGORIES,
-        override_existing: bool = False,
-    ) -> pathlib.Path | None:
-        response = requests.get(self.proxy_url + LEGACY_MODEL_GITHUB_URLS[model_category_name])
-        target_file_path = get_model_reference_file_path(model_category_name, base_path=self.legacy_path)
-
-        if target_file_path.exists() and not override_existing:
-            return None
-
-        target_file_path.parent.mkdir(parents=True, exist_ok=True)
-        with open(target_file_path, "wb") as f:
-            f.write(response.content)
-        return target_file_path
-
-    def download_all_legacy_model_references(
-        self,
-        *,
-        override_existing: bool = False,
-    ) -> dict[MODEL_REFERENCE_CATEGORIES, pathlib.Path | None]:
-        """Download all legacy model reference files from https://github.com/db0/AI-Horde-image-model-reference.
-
-        Args:
-            override_existing (bool, optional): If true, overwrite any existing files. Defaults to False.
-
-        Returns:
-            dict[MODEL_REFERENCE_CATEGORIES, Path | None]: The files written, or `None` if that reference failed
-        """
-        downloaded_files: dict[MODEL_REFERENCE_CATEGORIES, pathlib.Path | None] = {}
-        for model_category_name in MODEL_REFERENCE_CATEGORIES:
-            downloaded_files[model_category_name] = self.download_legacy_model_reference(
-                model_category_name=model_category_name,
-                override_existing=override_existing,
-            )
-
-        return downloaded_files
-
-    def read_all_legacy_model_references(
-        self,
-        *,
-        redownload_all: bool = False,
-    ) -> dict[MODEL_REFERENCE_CATEGORIES, Path | None]:
-        """Read all legacy model reference files from disk, optionally redownloading them first."""
-        return self.download_all_legacy_model_references(override_existing=redownload_all)
+from horde_model_reference.legacy.legacy_download_manager import LegacyReferenceDownloadManager
+from horde_model_reference.meta_consts import MODEL_REFERENCE_CATEGORY
 
 
 def download_all_models(
     override_existing: bool = False,
     proxy_url: str = "",
-) -> dict[MODEL_REFERENCE_CATEGORIES, pathlib.Path | None]:
-    reference_dm = ReferenceDownloadManager(proxy_url=proxy_url)
-    logger.warning("This method is deprecated. Use `download_all_model_references` instead.")
-    return reference_dm.download_all_legacy_model_references(override_existing=override_existing)
+) -> dict[MODEL_REFERENCE_CATEGORY, pathlib.Path | None]:
+    reference_dm = LegacyReferenceDownloadManager(proxy_url=proxy_url)
+    warnings.warn(
+        "download_all_models() is deprecated, use the class `LegacyReferenceDownloadManager` instead.",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    return reference_dm.download_all_legacy_model_references(overwrite_existing=override_existing)
 
 
 if __name__ == "__main__":
-    reference_download_manager = ReferenceDownloadManager()
-    reference_download_manager.download_all_legacy_model_references(override_existing=True)
+    reference_download_manager = LegacyReferenceDownloadManager()
+    reference_download_manager.download_all_legacy_model_references(overwrite_existing=True)
---
+++ tests/test_model_reference_manager.py
@@ -0,0 +1,26 @@
+from horde_model_reference.meta_consts import MODEL_REFERENCE_CATEGORY
+from horde_model_reference.model_reference_manager import ModelReferenceManager
+
+
+def test_manager_init():
+    ModelReferenceManager()
+
+
+def test_manager_legacy():
+    model_reference_manager = ModelReferenceManager()
+
+    legacy_reference_locations = model_reference_manager.get_all_legacy_model_references()
+
+    assert len(legacy_reference_locations) > 0
+    assert MODEL_REFERENCE_CATEGORY.stable_diffusion in legacy_reference_locations
+
+
+def test_manager_new_format():
+    model_reference_manager = ModelReferenceManager()
+
+    all_model_references = model_reference_manager.get_all_model_references()
+
+    assert len(all_model_references) > 0
+
+    for model_reference_category in MODEL_REFERENCE_CATEGORY:
+        assert model_reference_category in all_model_references
