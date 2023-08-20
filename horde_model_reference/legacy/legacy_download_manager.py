a18ded111a9c6878c4f4bfba38b1d55b3df22f08
tazlin <tazlin.on.github@gmail.com>
2023-08-20 08:27:53 -0400

    feat: ModelReference and LegacyDownload managers
    These will hopefully serve as more straight forward interfaces to the library.

---
+++ horde_model_reference/legacy/legacy_download_manager.py
@@ -0,0 +1,96 @@
+import pathlib
+from pathlib import Path
+
+import requests
+from loguru import logger
+
+from horde_model_reference.legacy.convert_all_legacy_dbs import convert_all_legacy_model_references
+from horde_model_reference.meta_consts import MODEL_REFERENCE_CATEGORY
+from horde_model_reference.path_consts import (
+    BASE_PATH,
+    LEGACY_MODEL_GITHUB_URLS,
+    LEGACY_REFERENCE_FOLDER_NAME,
+    get_model_reference_file_path,
+)
+
+
+class LegacyReferenceDownloadManager:
+    base_path: str | Path = BASE_PATH
+    """The base path to use for all file operations."""
+    legacy_path: Path
+    """The path to the legacy reference folder."""
+
+    proxy_url: str = ""
+    """The URL to use as a proxy for downloading files. If empty, no proxy will be used."""
+
+    _cached_file_locations: dict[MODEL_REFERENCE_CATEGORY, pathlib.Path | None] | None = None
+
+    def __init__(
+        self,
+        *,
+        base_path: str | Path = BASE_PATH,
+        proxy_url: str = "",
+    ) -> None:
+        self.base_path = base_path
+        self.legacy_path = Path(self.base_path).joinpath(LEGACY_REFERENCE_FOLDER_NAME)
+        self.proxy_url = proxy_url
+
+    def download_legacy_model_reference(
+        self,
+        *,
+        model_category_name: MODEL_REFERENCE_CATEGORY,
+        override_existing: bool = False,
+    ) -> pathlib.Path | None:
+        response = requests.get(self.proxy_url + LEGACY_MODEL_GITHUB_URLS[model_category_name])
+        target_file_path = get_model_reference_file_path(model_category_name, base_path=self.legacy_path)
+
+        if target_file_path.exists() and not override_existing:
+            logger.debug(f"File {target_file_path} already exists, skipping download.")
+            return None
+
+        target_file_path.parent.mkdir(parents=True, exist_ok=True)
+        with open(target_file_path, "wb") as f:
+            f.write(response.content)
+        return target_file_path
+
+    def download_all_legacy_model_references(
+        self,
+        *,
+        overwrite_existing: bool = True,
+    ) -> dict[MODEL_REFERENCE_CATEGORY, pathlib.Path | None]:
+        """Download all legacy model reference files from https://github.com/db0/AI-Horde-image-model-reference.
+
+        Args:
+            override_existing (bool, optional): If true, overwrite any existing files. Defaults to False.
+
+        Returns:
+            dict[MODEL_REFERENCE_CATEGORY, Path | None]: The files written, or `None` if that reference failed
+        """
+        downloaded_files: dict[MODEL_REFERENCE_CATEGORY, pathlib.Path | None] = {}
+        for model_category_name in MODEL_REFERENCE_CATEGORY:
+            downloaded_files[model_category_name] = self.download_legacy_model_reference(
+                model_category_name=model_category_name,
+                override_existing=overwrite_existing,
+            )
+
+        return downloaded_files
+
+    def get_all_legacy_model_references(
+        self,
+        *,
+        redownload_all: bool = False,
+    ) -> dict[MODEL_REFERENCE_CATEGORY, Path | None]:
+        """Read all legacy model reference files from disk, optionally redownloading them first."""
+        if not redownload_all and self._cached_file_locations:
+            return self._cached_file_locations
+
+        self._cached_file_locations = self.download_all_legacy_model_references(overwrite_existing=redownload_all)
+
+        return self._cached_file_locations
+
+    def convert_legacy_references(self):
+        """Convert all legacy model reference files to the new format."""
+        convert_all_legacy_model_references(
+            base_path=self.base_path,
+            legacy_path=self.legacy_path,
+        )
---
+++ horde_model_reference/model_reference_manager.py
@@ -0,0 +1,111 @@
+import json
+from pathlib import Path
+
+import horde_model_reference.path_consts as path_consts
+from horde_model_reference.legacy.convert_all_legacy_dbs import convert_all_legacy_model_references
+from horde_model_reference.legacy.download_live_legacy_dbs import LegacyReferenceDownloadManager
+from horde_model_reference.model_reference_records import (
+    MODEL_REFERENCE_TYPE_LOOKUP,
+    Generic_ModelReference,
+)
+from horde_model_reference.path_consts import MODEL_REFERENCE_CATEGORY
+
+
+class ModelReferenceManager:
+    """Class for downloading and reading model reference files."""
+
+    _legacy_reference_download_manager: LegacyReferenceDownloadManager
+    _cached_new_references: dict[MODEL_REFERENCE_CATEGORY, Generic_ModelReference | None] = {}
+
+    def __init__(
+        self,
+        download_and_convert_legacy_dbs: bool = True,
+        override_existing: bool = True,
+    ) -> None:
+        """
+        Initialize a new ModelReferenceManager instance.
+
+        Args:
+            download_and_convert_legacy_dbs: Whether to download and convert legacy model references.
+            override_existing: Whether to override existing model reference files.
+        """
+        self._legacy_reference_download_manager = LegacyReferenceDownloadManager()
+        if download_and_convert_legacy_dbs:
+            self.download_and_convert_all_legacy_dbs(override_existing)
+
+    def download_and_convert_all_legacy_dbs(self, override_existing: bool = True) -> None:
+        """
+        Download and convert all legacy model reference files.
+
+        Args:
+            override_existing: Whether to override existing model reference files.
+        """
+        self._legacy_reference_download_manager.download_all_legacy_model_references(
+            overwrite_existing=override_existing,
+        )
+        convert_all_legacy_model_references(
+            self._legacy_reference_download_manager.base_path,
+            self._legacy_reference_download_manager.legacy_path,
+        )
+
+    def get_all_legacy_model_references(
+        self,
+        redownload_all: bool = False,
+    ) -> dict[MODEL_REFERENCE_CATEGORY, Path | None]:
+        """
+        Get all legacy model reference files.
+
+        Args:
+            redownload_all: Whether to redownload all legacy model reference files.
+
+        Returns:
+            A dictionary mapping model reference categories to file paths.
+        """
+        return self._legacy_reference_download_manager.get_all_legacy_model_references(
+            redownload_all=redownload_all,
+        )
+
+    def get_all_model_references(
+        self,
+        redownload_all_legacy: bool = False,
+    ) -> dict[MODEL_REFERENCE_CATEGORY, Generic_ModelReference | None]:
+        """
+        Get all model reference files.
+
+        Args:
+            redownload_all_legacy: Whether to redownload all legacy model reference files.
+
+        Returns:
+            A dictionary mapping model reference categories to file paths.
+        """
+
+        if not redownload_all_legacy and self._cached_new_references:
+            return self._cached_new_references
+
+        if redownload_all_legacy:
+            self.download_and_convert_all_legacy_dbs()
+
+        all_files: dict[MODEL_REFERENCE_CATEGORY, Path | None] = path_consts.get_all_model_reference_file_paths()
+
+        self._cached_new_references: dict[MODEL_REFERENCE_CATEGORY, Generic_ModelReference | None] = {}
+
+        for category, file_path in all_files.items():
+            if file_path is None:
+                self._cached_new_references[category] = None
+            else:
+                with open(file_path) as f:
+                    file_contents = f.read()
+                file_json: dict = json.loads(file_contents)
+
+                parsed_model = MODEL_REFERENCE_TYPE_LOOKUP[category].model_validate(file_json)
+
+                self._cached_new_references[category] = parsed_model
+
+        return_dict: dict[MODEL_REFERENCE_CATEGORY, Generic_ModelReference | None] = {}
+        for reference_type, reference in self._cached_new_references.items():
+            if reference is None:
+                return_dict[reference_type] = None
+                continue
+            return_dict[reference_type] = reference.model_copy(deep=True)
+
+        return return_dict
