10b057a0fa1132d5ade64346186431fc9cf00143
tazlin <tazlin.on.github@gmail.com>
2024-11-24 13:54:28 -0500

    fix: change name from SD specific to a general one
    At this point, we no longer only support Stable Diffusion models.

--- src/horde_model_reference/meta_consts.py
+++ src/horde_model_reference/meta_consts.py
@@ -66,10 +66,12 @@ class MODEL_REFERENCE_CATEGORY(StrEnum):
     ti = auto()
     lora = auto()
 
+
 LOCAL_MODEL_REFERENCE_CATEGORIES = [
     MODEL_REFERENCE_CATEGORY.ti,
     MODEL_REFERENCE_CATEGORY.lora,
 ]
+
 
 class MODEL_PURPOSE(StrEnum):
     image_generation = auto()
